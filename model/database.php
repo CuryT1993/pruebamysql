32597b8ced6e431513d657081a58fa4638edb686
CuryT1993 <curytravez_t.v@hotmail.com>
2018-05-10 10:30:37 -0500

    conexion
    conexion

--- model/database.php
+++ model/database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function Conectar()
     {
-        $pdo = new PDO('mysql:curytravez.mysql.database.azure.com;dbname=bdcury;charset=utf8', 'curytravez@curytravez', 'Atahualpacury1993');
+        $pdo = new PDO('mysql:servidorcury.mysql.database.azure.com;dbname=bdcury;charset=utf8', 'curyt@servidorcury', 'Atahualpacury1993');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
 
