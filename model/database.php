f7accf1c5d87b49d03af6691618ec3e20e7b352d
CuryT1993 <curytravez_t.v@hotmail.com>
2018-05-23 23:01:18 -0500

    cambio de cadena
    mysql

--- model/database.php
+++ model/database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function Conectar()
     {
-        $pdo = new PDO('mysql:host=servidorcury.mysql.database.azure.com;dbname=bdcury;charset=utf8', 'curyt@servidorcury', 'Atahualpacury1993');
+        $pdo = new PDO('mysql:host=curyservidor.mysql.database.azure.com;dbname=bdcury;charset=utf8', 'curytravez@curyservidor', 'Atahualpacury1993');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
 
